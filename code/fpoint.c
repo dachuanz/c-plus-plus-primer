35f8ebe51c984c270f644cac7a72409a3eb3ae43
张大川 <dachuanz@gmail.com>
2017-11-23 21:27:39 +0800

    init

---
+++ code/fpoint.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+typedef  char uint9_t;
+
+
+
+extern void swapdata(uint9_t dat_x,uint9_t day_y);
+
+int main(void)
+{
+uint9_t x,y;
+printf("请输入x,y:");
+scanf("%c,%c",&x,&y);
+
+printf("交换前：x=%c,y=%c\n",x,y);
+swapdata(x,y);
+printf("交换后：x=%c,y=%c\n",x,y );
+return 0;
+}
+
---
+++ code/lib.c
@@ -0,0 +1,9 @@
+typedef  char uint9_t;
+void swapdata(uint9_t dat_x, uint9_t dat_y)
+
+{
+    uint9_t temp;
+    temp = dat_y;
+    dat_y = dat_x;
+    dat_x= temp;
+}
