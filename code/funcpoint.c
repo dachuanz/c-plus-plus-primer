27a074af525b74d7d09e978e1ef65d082800223f
张大川 <dachuanz@gmail.com>
2017-11-23 18:37:21 +0800

    init

---
+++ code/funcpoint.c
@@ -0,0 +1,27 @@
+﻿# include <stdio.h>
+# include <string.h>
+
+void check(char* a, char* b, int(*cmp)(const char*, const char*));
+
+int main( void)
+{
+    char s1[80], s2[80];
+    int(*p)(const char*, const char*);     
+    p = strcmp;   
+    printf("输入两个字符串:\n");
+    gets(s1);    
+    gets(s2);   
+}
+
+void check(char* a, char* b, int(*cmp)(const char*, const char*))
+{
+    printf("测试是否相等\n");
+    if(!cmp(a, b))
+        {
+            printf("结果：相等\n");
+        }
+    else
+        {
+            printf("结果：不相等\n");
+        }
+}
