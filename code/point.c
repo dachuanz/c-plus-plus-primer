828b35175580ff6e8a5a83b738811e6592970624
张大川 <dachuanz@gmail.com>
2017-11-23 18:59:09 +0800

    init

---
+++ code/point.c
@@ -0,0 +1,16 @@
+﻿# include <stdio.h>
+
+int main(void)
+{
+    int* p;
+    int begin, end;
+    begin = 10;
+    p = &begin;
+    end = *p;
+    printf("begin = %d\n", begin);
+    printf("end = %d\n", end);
+    printf("p = %p\n", p);
+    printf("%zd\n", sizeof(p));
+    printf("*p = %d\n", *p);
+    return 0;
+}
