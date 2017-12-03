9101b4adbd0e828bb5f167938aeb422e544c5f80
张大川 <dachuanz@gmail.com>
2017-12-03 16:57:41 +0800

    updatw

--- code/lim.cpp
+++ code/lim.cpp
@@ -9,11 +9,11 @@ int main()
 
 {
     long  l_min = LONG_MIN;//long int 简写为long
-    short s_min=SHRT_MIN;//short int 简写为 short 
+    short s_min = SHRT_MIN; //short int 简写为 short
     long long  ll_min = LLONG_MIN;//long long int 简写为long long
     long long  ll_max = LLONG_MAX;
     unsigned long long  ull_max = ULLONG_MAX;
-    printf ("min of short:%d\n",s_min);
+    printf ("min of short:%d\n", s_min);
     printf("min of long:%ld\n", l_min);
     printf("min of long long : %lld\n", ll_min);
     printf("max of long long : %lld\n", ll_max);
