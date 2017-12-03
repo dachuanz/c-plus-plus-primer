a9faf407bc23a8b9188a4764e487e51734c69c08
张大川 <dachuanz@gmail.com>
2017-12-03 17:11:21 +0800

    update

--- code/lim.cpp
+++ code/lim.cpp
@@ -8,11 +8,18 @@ using namespace std;
 int main()
 
 {
+    // singed 标识符一般省略
+    char c_min = SCHAR_MIN;
+    char c_max = SCHAR_MAX;
+    unsigned char uc_max = UCHAR_MAX;
     long  l_min = LONG_MIN;//long int 简写为long
     short s_min = SHRT_MIN; //short int 简写为 short
     long long  ll_min = LLONG_MIN;//long long int 简写为long long
     long long  ll_max = LLONG_MAX;
     unsigned long long  ull_max = ULLONG_MAX;
+    printf ("max of  unsigned char:%d\n", uc_max);
+    printf("min of char:%d\n", c_min);
+    printf("max of char:%d\n", c_max);
     printf("min of short:%d\n", s_min);
     printf("min of long:%ld\n", l_min);
     printf("min of long long : %lld\n", ll_min);
